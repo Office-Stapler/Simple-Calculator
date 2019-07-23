52eb89bf7a124d2209d6a7d78d7453711001b499
Raymond Lam <53181007+Office-Stapler@users.noreply.github.com>
2019-07-23 10:40:02 +1000

    Update
    - Added the Matrix struct type which consists of 3 vectors.
    - Changed addVectors function to return a vector type which can be used for other functions.
    - Added a scanMatrix function to scan the rows and coloumns of a 3x3 vector
    - Changed Projection function to take vector arguments allowing for further use in future functions

--- Simple Calculator.c
+++ Simple Calculator.c
@@ -9,12 +9,20 @@ typedef struct vector {
 	int z;
 } Vector;
 
-void cross_product(void);
-int dot_product(Vector v1, Vector v2);
+typedef struct matrix{
+	Vector v1;
+	Vector v2;
+	Vector v3;
+} Matrix;
+
+void crossProduct(void);
+int dotProduct(Vector v1, Vector v2);
 double magnitude(Vector v1);
-void add_vectors(void);
-void scale_vector(void);
-void projection(void);
+Vector addVectors(Vector v1, Vector v2);
+void scaleVector(void);
+void projection(Vector v1, Vector v2);
+Matrix matrixAddition(Matrix m1, Matrix m2);
+Matrix scanMatrix(void);
 Vector scanVector(void);
 
 int main(void) {
@@ -25,12 +33,13 @@ int main(void) {
 	printf("Enter 4 to add two 3D vectors\n");
 	printf("Enter 5 to scale a vector by a number\n");
 	printf("Enter 6 to find the projection of vector v1 on v2\n");
+	printf("Enter 7 to add two 3x3 matricies\n");
 	printf("Enter a command: ");
 	char c = getchar();
 	int i = 0;
 	for (; c != '0'; c = getchar() ) {
 		if (c == '1') {
-			cross_product();
+			crossProduct();
 		} else if (c == '2') {
 			Vector v1;
 			double magnitude1;
@@ -44,18 +53,37 @@ int main(void) {
 			v1 = scanVector();
 			printf("Enter vector 2:\n");
 			v2 = scanVector();
-			int product = dot_product(v1,v2);
+			int product = dotProduct(v1,v2);
 			printf("The dot product of v1 and v2 is %d\n", product);
 		} else if (c == '4') {
-			add_vectors();
+			Vector v1,v2,v3;
+			printf("Enter vector 1:\n");		
+			v1 = scanVector();
+			printf("Enter vector 2:\n");
+			v2 = scanVector();
+			v3 = addVectors(v1,v2);
+			printf("The sum of v1 and v2 is (%d %d %d)\n", v3.x , v3.y, v3.z);
 		} else if (c == '5') {
-			scale_vector();
-		} 
-		#if 1
-		else if (c == '6') {
-			projection();
-		} 
-		#endif	
+			scaleVector();
+		} else if (c == '6') {
+			Vector v1,v2;
+			printf("Enter vector 1:\n");		
+			v1 = scanVector();
+			printf("Enter vector 2:\n");
+			v2 = scanVector();
+			projection(v1,v2);
+		} else if (c == '7') {
+			Matrix m1, m2,m3;
+			printf("Matrix 1\n");
+			m1 = scanMatrix();
+			printf("Matrix 2\n");
+			m2 = scanMatrix();
+			m3 = matrixAddition(m1, m2);
+			printf("The sum of the two matricies is:\n");
+			printf("%d %d %d\n", m3.v1.x, m3.v2.x, m3.v3.x);
+			printf("%d %d %d\n", m3.v1.x, m3.v2.x, m3.v3.x);
+			printf("%d %d %d\n", m3.v1.x, m3.v2.x, m3.v3.x);
+		}
 		if (c != '\n') {
 			printf("Enter a command: ");
 		}
@@ -63,7 +91,7 @@ int main(void) {
 	printf("Goodbye!\n");
 }
 
-void cross_product(void) {
+void crossProduct(void) {
 	Vector v1,v2;
 	printf("Enter vector 1:\n");		
 	v1 = scanVector();
@@ -82,26 +110,22 @@ double magnitude(Vector v1) {
 	return magnitude;
 }
 
-int dot_product(Vector v1, Vector v2) {
+int dotProduct(Vector v1, Vector v2) {
 	int product;
 	product = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 	return product;
 	
 }
 
-void add_vectors(void) {
-	Vector v1,v2,v3;
-	printf("Enter vector 1:\n");		
-	v1 = scanVector();
-	printf("Enter vector 2:\n");
-	v2 = scanVector();
+Vector addVectors(Vector v1, Vector v2) {
+	Vector v3;
 	v3.x = v1.x + v2.x;
 	v3.y = v1.y + v2.y;
 	v3.z = v1.z + v2.z;
-	printf("The sum of v1 and v2 is (%d %d %d)\n", v3.x , v3.y, v3.z);
+	return v3;
 }
 
-void scale_vector(void) {
+void scaleVector(void) {
 	Vector v1,v2;
 	int scale;
 	printf("Enter vector 1:\n");		
@@ -113,17 +137,12 @@ void scale_vector(void) {
 	v2.z = v1.z * scale;
 	printf("v1 scaled by %d is (%d %d %d)\n", scale,  v2.x , v2.y, v2.z);
 }
-#if 1
-void projection(void) {
-	Vector v1,v2;
-	printf("Enter vector 1:\n");		
-	v1 = scanVector();
-	printf("Enter vector 2:\n");
-	v2 = scanVector();
+
+void projection(Vector v1, Vector v2) {
 	int magnitude1;
 	int product;
 	magnitude1 = (int) pow(magnitude(v2),2);
-	product = dot_product(v1,v2);
+	product = dotProduct(v1,v2);
 	for(int i = 2; i <= (magnitude1 / 2) || i <= (product / 2); i++) {
 		if (product % i == 0 && magnitude1 % i == 0) {
 			product /= i;
@@ -134,7 +153,25 @@ void projection(void) {
 	}
 	printf("The projection of v1 on v2 is %d / %d * (%d %d %d)\n", product, magnitude1, v2.x , v2.y, v2.z);
 }
-#endif
+
+Matrix matrixAddition(Matrix m1, Matrix m2) {
+	Matrix m3;
+	m3.v1 = addVectors(m1.v1, m2.v1);
+	m3.v2 = addVectors(m1.v2, m2.v2);
+	m3.v3 = addVectors(m1.v3, m2.v3);
+	return m3;
+}
+
+Matrix scanMatrix(void) {
+	Matrix m1;
+	printf("Enter the values of row 1: ");
+	scanf("%d %d %d",&m1.v1.x, &m1.v2.x, &m1.v3.x);
+	printf("Enter the values of row 2: ");
+	scanf("%d %d %d",&m1.v1.y, &m1.v2.y, &m1.v3.y);
+	printf("Enter the values of row 3: ");
+	scanf("%d %d %d",&m1.v1.z, &m1.v2.z, &m1.v3.z);
+	return m1;
+}
 Vector scanVector(void) {
 	Vector v1;
 	scanf("%d %d %d",&(v1.x), &(v1.y), &(v1.z));
